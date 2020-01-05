db0384c65d513abc53a943b5b665204f94f8eafb
Ajewole Adedamola <adedamolaajewole@gmail.com>
2020-01-06 00:09:33 +0100

    plot

--- statsandmathsprocessing.py
+++ statsandmathsprocessing.py
@@ -5,11 +5,11 @@ axes_values = np.arange(-100,100,10)
 
 dx, dy = np.meshgrid(axes_values,axes_values)
 
-print ("dx:")
-print (dx)
+#print ("dx:")
+#print (dx)
 
-print ("dy")
-print (dy)
+#print ("dy")
+#print (dy)
 
 function1 = 2*dx+3*dy
 function2 = np.cos(dx)+np.cos(dy)
@@ -22,5 +22,5 @@ print (function2)
 plt.imshow(function2)
 plt.title("function cos plot")
 plt.colorbar()
-plt.savefig('myfig2.png')
+plt.savefig('myplot.png')
 
