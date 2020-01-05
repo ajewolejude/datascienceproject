68881a35fb883c458a3849ec7b5dc78b1709a55d
Ajewole Adedamola <adedamolaajewole@gmail.com>
2020-01-06 00:18:18 +0100

    plot

--- statsandmathsprocessing.py
+++ statsandmathsprocessing.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-axes_values = np.arange(-100,100,10)
+axis_values = np.arange(-100,100,10)
 
-dx, dy = np.meshgrid(axes_values,axes_values)
+xaxis, yaxis = np.meshgrid(axis_values,axis_values)
 
 #print ("dx:")
 #print (dx)
@@ -11,13 +11,13 @@ dx, dy = np.meshgrid(axes_values,axes_values)
 #print ("dy")
 #print (dy)
 
-function1 = 2*dx+3*dy
-function2 = np.cos(dx)+np.cos(dy)
+function1 = 2*xaxis+3*yaxis
+function2 = np.cos(xaxis)+np.cos(yaxis)
 
 print (function1)
 print (function2)
 
-#replace function2 by function1 to get graph of function1
+#replace function2 by function1 to get graph of function1.
 #plotting the function on graph
 plt.imshow(function2)
 plt.title("function cos plot")
